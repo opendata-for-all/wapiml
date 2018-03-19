bf102a164e86630cb8a0bc4da030cfae2fad2f79
hamza <hamza.eddouibi@gmail.com>
2018-03-19 18:01:38 +0100

    init test blufin

---
+++ plugins/edu.uoc.som.openapitouml.tests/src/edu/uoc/som/openapitouml/tests/TestOpenAPIToUML.java
@@ -0,0 +1,14 @@
+package edu.uoc.som.openapitouml.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestOpenAPIToUML {
+
+	@Test
+	void testGenerateClassDiagramFileString() {
+		fail("Not yet implemented");
+	}
+
+}
