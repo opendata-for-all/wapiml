d997236fc45a841f7ebfcd8188c4a5db73918afc
hamza <hamza.eddouibi@gmail.com>
2018-03-20 18:58:05 +0100

    Exception classes

---
+++ plugins/edu.uoc.som.openapitouml/src/edu/uoc/som/openapitouml/exception/OpenAPIValidationException.java
@@ -0,0 +1,16 @@
+package edu.uoc.som.openapitouml.exception;
+
+public class OpenAPIValidationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OpenAPIValidationException(String message) {
+		super(message);
+		
+	}
+
+	
+}
