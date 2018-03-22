e1f9127ea2b559d18b0473e21ec8ba213910d2bf
hamza <hamza.eddouibi@gmail.com>
2018-03-22 12:06:00 +0100

    update the tests

--- plugins/edu.uoc.som.openapitouml.tests/src/edu/uoc/som/openapitouml/tests/TestOpenAPIValidator.java
+++ plugins/edu.uoc.som.openapitouml.tests/src/edu/uoc/som/openapitouml/tests/TestOpenAPIValidator.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.uml2.uml.Model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,7 +18,7 @@ import edu.uoc.som.openapitouml.facade.OpenAPItoUMLFacade;
 @DisplayName("Test Open API validator")
 class TestOpenAPIValidator {
 
-	static final Logger LOGGER = LogManager.getLogger(TestOpenAPIValidator.class);
+	static final Logger LOG = LogManager.getLogger(TestOpenAPIValidator.class);
 	@DisplayName("Test generate model using valid def")
 	@Test
 	void testGenerateClassDiagramValidDef() {
@@ -46,7 +44,7 @@ class TestOpenAPIValidator {
 	    	 openAPItoUMLFacade.generateClassDiagram(input, "petstore", true);
 	    });
 		
-	       LOGGER.info(exception.getMessage());
+	       LOG.info(exception.getMessage());
 	}
 	
 	
