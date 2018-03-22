8fc3c46512a6d5f53c6033c0834a2ed71012c63d
hamza <hamza.eddouibi@gmail.com>
2018-03-22 12:07:40 +0100

    adapt the new UI plugin

--- plugins/edu.uoc.som.openapitouml.ui/src/edu/uoc/som/openapitouml/ui/handlers/ConvertToClassDiagram.java
+++ plugins/edu.uoc.som.openapitouml.ui/src/edu/uoc/som/openapitouml/ui/handlers/ConvertToClassDiagram.java
@@ -16,11 +16,17 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import edu.uoc.som.openapitouml.generators.OpenAPItoUML;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+
+import edu.uoc.som.openapitouml.exception.OpenAPIValidationException;
+import edu.uoc.som.openapitouml.facade.OpenAPItoUMLFacade;
 import edu.uoc.som.openapitouml.ui.OpenAPIToUMLUIPlugin;
 
 public class ConvertToClassDiagram extends AbstractHandler {
@@ -35,34 +41,48 @@ public class ConvertToClassDiagram extends AbstractHandler {
 			Job job = new Job(ID) {
 				@Override
 				protected IStatus run(IProgressMonitor monitor) {
-					if (monitor == null) monitor = new NullProgressMonitor();
+					final StringBuilder message = new StringBuilder();
+					if (monitor == null)
+						monitor = new NullProgressMonitor();
 					try {
+
 						monitor.beginTask("Generationg the Class diagram", IProgressMonitor.UNKNOWN);
+
 						for (Iterator<?> iterator = structuredSelection.iterator(); iterator.hasNext();) {
-							
+
 							Object obj = iterator.next();
 							if (obj instanceof IFile) {
 								IFile iFile = (IFile) obj;
+
 								IContainer target = iFile.getProject().getFolder("src-gen");
 								if (!target.getLocation().toFile().exists()) {
-									target.getLocation().toFile().mkdirs();	
+									target.getLocation().toFile().mkdirs();
 									iFile.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
 								}
-								OpenAPItoUML.genrateAndSaveClassDiagram(new File(iFile.getLocation().toString()),iFile.getName().substring(0, iFile.getName().lastIndexOf('.')) , target.getFullPath().toString());
+								OpenAPItoUMLFacade openAPItoUMLFacade = new OpenAPItoUMLFacade();
+								File inputFile = new File(iFile.getLocation().toString());
 
+								openAPItoUMLFacade.generateAndSaveClassDiagram(inputFile,
+										iFile.getName().substring(0, iFile.getName().lastIndexOf('.')),
+										target.getLocation().toFile(), true);
+								iFile.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
 							}
 						}
-					} catch (IOException | CoreException e) {
-						return new Status(IStatus.ERROR, OpenAPIToUMLUIPlugin.PLUGIN_ID, e.getLocalizedMessage(), e);
-						
+					} catch (IOException | CoreException | ProcessingException e) {
+						return new Status(IStatus.ERROR, OpenAPIToUMLUIPlugin.PLUGIN_ID, e.getLocalizedMessage(),
+								e.getCause());
+
+					} catch (OpenAPIValidationException e) {
+						return new Status(IStatus.ERROR, OpenAPIToUMLUIPlugin.PLUGIN_ID, e.getLocalizedMessage());
 					} finally {
 						monitor.done();
 					}
+
 					return Status.OK_STATUS;
 				}
 			};
 			job.schedule();
-			
+
 		}
 		return null;
 	}
