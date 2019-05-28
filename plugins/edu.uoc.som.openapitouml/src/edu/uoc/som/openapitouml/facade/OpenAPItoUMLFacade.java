ce630668555a298547fb2bc4c5ca4e243d9d215d
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-05-29 01:01:10 +0200

    Delete unused import

--- plugins/edu.uoc.som.openapitouml/src/edu/uoc/som/openapitouml/facade/OpenAPItoUMLFacade.java
+++ plugins/edu.uoc.som.openapitouml/src/edu/uoc/som/openapitouml/facade/OpenAPItoUMLFacade.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.uml2.uml.Model;
 
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
