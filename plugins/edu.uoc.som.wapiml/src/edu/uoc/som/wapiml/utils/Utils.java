e28351990b1d2715673540c270713bfd9c70218e
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-07-30 17:17:48 +0200

    Extend Utils to support Yaml format as well

--- plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/utils/Utils.java
+++ plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/utils/Utils.java
@@ -2,6 +2,7 @@ package edu.uoc.som.wapiml.utils;
 
 import java.io.File;
 import java.io.IOException;
+
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 
 import edu.uoc.som.openapi2.API;
@@ -15,20 +16,32 @@ import edu.uoc.som.wapiml.validator.OpenAPIValidator;
 
 public class Utils {
 	
-	public static API loadOpenAPIModel(File inputFile) throws IOException, ProcessingException {
+	public static API loadOpenAPIModel(File inputFile, SerializationFormat serializationFormat) throws IOException, ProcessingException {
+		if(serializationFormat.equals(SerializationFormat.JSON)) {
 		OpenAPIValidator openAPIValidator = new OpenAPIValidator();
 		OpenAPIValidationReport report = openAPIValidator.validate(inputFile);
 		if(!report.isSuccess()){
 			throw new OpenAPIValidationException("Invalid OpenAPI definition\n"+report.getError());
 		}
 		
-	return new OpenAPI2Importer().createOpenAPI2ModelFromFile(inputFile, SerializationFormat.JSON);
+	return new OpenAPI2Importer().createOpenAPI2ModelFromFile(inputFile, SerializationFormat.JSON);}
+		if(serializationFormat.equals(SerializationFormat.YAML)) {
+			return new OpenAPI2Importer().createOpenAPI2ModelFromFile(inputFile, SerializationFormat.YAML);
+		}
+		return null;
 	}
-	public static void convertAndSaveOpenAPIDefinition(File inputFile, File targetFile) throws IOException {
+	public static void convertAndSaveOpenAPIDefinition(File inputFile, File targetFile, SerializationFormat serializationFormat) throws IOException {
 		OpenAPIModelGenerator openAPIModelGenerator = new OpenAPIModelGenerator(inputFile);
 		API api = openAPIModelGenerator.generate();
+		if(serializationFormat.equals(SerializationFormat.JSON)) {
 		String jsonDefinition = new ExporterBuilder().setJsonPrettyPrinting().exportJson(api);
 		edu.uoc.som.openapi2.io.utils.Utils.saveOpenAPIDefintion(jsonDefinition, targetFile);
+		}
+		if(serializationFormat.equals(SerializationFormat.YAML)) {
+			String yamlDefinition = new ExporterBuilder().setJsonPrettyPrinting().exportYaml(api);
+			edu.uoc.som.openapi2.io.utils.Utils.saveOpenAPIDefintion(yamlDefinition, targetFile);
+			}
+		
 	}
 
 }
