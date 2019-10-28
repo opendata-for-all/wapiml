3fbf1e8cd7be4c85d9337eb703e65aa734368fa6
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-10-28 11:27:48 +0100

    Load resources depending on the context (eclipse or standalone)
    this will prepare having a version which does not depend on Eclipse
    (e.g., web app)

--- plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/utils/UMLUtils.java
+++ plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/utils/UMLUtils.java
@@ -1,6 +1,13 @@
 package edu.uoc.som.wapiml.utils;
 
+import java.io.File;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.uml2.uml.AggregationKind;
+import org.eclipse.uml2.uml.UMLPackage;
+import org.eclipse.uml2.uml.resource.UMLResource;
 
 public class UMLUtils {
 
@@ -15,6 +22,53 @@ public class UMLUtils {
 		default:
 			return null;
 		}
+	}
+	
+	
+	public static ResourceSet initUMLResourceSet(boolean standalone) {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		
+		
+
+	
+		resourceSet.getPackageRegistry().put(UMLPackage.eNS_URI, UMLPackage.eINSTANCE);
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(UMLResource.FILE_EXTENSION,
+				UMLResource.Factory.INSTANCE);
+		if (standalone) {
+			File baseURIFolder = new File(UMLUtils.class.getResource("resources").getFile());
+			URI baseUri = URI.createFileURI(baseURIFolder.getAbsolutePath());
+			resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.LIBRARIES_PATHMAP),
+					baseUri.appendSegment("libraries").appendSegment(""));
+			resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.METAMODELS_PATHMAP),
+					baseUri.appendSegment("metamodels").appendSegment(""));
+			resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.PROFILES_PATHMAP),
+					baseUri.appendSegment("profiles").appendSegment(""));
+			resourceSet.getURIConverter().getURIMap().put(
+					URI.createURI("pathmap://OPENAPI_PROFILES/openapi.profile.uml"),
+					baseUri.appendSegment("openapi.profile").appendFileExtension("uml"));
+
+
+
+		} else {
+			
+			resourceSet.getURIConverter().getURIMap().put(
+					URI.createURI("pathmap://UML_LIBRARIES/UMLPrimitiveTypes.library.uml"), URI.createPlatformPluginURI(
+							"org.eclipse.uml2.uml.resources/libraries/UMLPrimitiveTypes.library.uml", true));
+			resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.LIBRARIES_PATHMAP),
+					URI.createPlatformPluginURI("org.eclipse.uml2.uml.resources", true).appendSegment("libraries")
+							.appendSegment(""));
+			resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.METAMODELS_PATHMAP),
+					URI.createPlatformPluginURI("org.eclipse.uml2.uml.resources", true).appendSegment("metamodels")
+							.appendSegment(""));
+			resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.PROFILES_PATHMAP),
+					URI.createPlatformPluginURI("org.eclipse.uml2.uml.resources", true).appendSegment("profiles")
+							.appendSegment(""));
+			resourceSet.getURIConverter().getURIMap().put(
+					URI.createURI("pathmap://OPENAPI_PROFILES/openapi.profile.uml"),
+					URI.createPlatformPluginURI("edu.uoc.som.openapi2.profile/resources/openapi.profile.uml", true));
+		}
+
+		return resourceSet;
 	}
 	
 }
