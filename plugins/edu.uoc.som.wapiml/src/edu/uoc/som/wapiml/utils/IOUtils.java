3316b116dd990663498de788dfe171855784df3e
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-10-29 17:36:27 +0100

    New IO helpers

--- plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/utils/IOUtils.java
+++ plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/utils/IOUtils.java
@@ -2,7 +2,11 @@ package edu.uoc.som.wapiml.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 import edu.uoc.som.openapi2.API;
 import edu.uoc.som.openapi2.io.ExporterBuilder;
@@ -25,7 +29,7 @@ public class IOUtils {
 	}
 
 	public static void convertAndSaveOpenAPIDefinition(File inputFile, File targetFile,
-			SerializationFormat serializationFormat) throws IOException {
+			SerializationFormat serializationFormat) throws IOException, URISyntaxException {
 		OpenAPIModelGenerator openAPIModelGenerator = new OpenAPIModelGenerator(inputFile);
 		API api = openAPIModelGenerator.generate();
 		if (serializationFormat.equals(SerializationFormat.JSON)) {
@@ -39,4 +43,26 @@ public class IOUtils {
 
 	}
 
+	public static URI getResourcesURI() throws URISyntaxException {
+		String resourceURIString = UMLUtils.class.getResource("").toURI().toString();
+		String baseURIString = resourceURIString.substring(0, resourceURIString.indexOf("!")+2);
+		System.out.println(baseURIString);
+		return URI.createURI(baseURIString).appendSegment("resources");
+	}
+	public static boolean isStandalone() {
+		System.out.println("boom");
+		ResourceSet resourceSet = new ResourceSetImpl();
+	
+
+		try {
+			resourceSet.getResource(
+					URI.createPlatformPluginURI("edu.uoc.som.openapi2.profile/resources/openapi.profile.uml", true),
+					true);
+			return false;
+		} catch (Exception e) {
+			return true;
+		}
+	}
+
+	
 }
