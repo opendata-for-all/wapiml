81e9c53a5c14989333544170cb0bf99ed2dd1956
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-10-29 17:40:43 +0100

    Get a ResourceSet instead instead of creating a new one

--- plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/utils/OpenAPIStereotypesUtils.java
+++ plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/utils/OpenAPIStereotypesUtils.java
@@ -4,6 +4,7 @@ package edu.uoc.som.wapiml.utils;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.uml2.uml.NamedElement;
 import org.eclipse.uml2.uml.Stereotype;
@@ -23,17 +24,18 @@ public class OpenAPIStereotypesUtils {
 		private static final URI OPENAPI_PROFILE_URI = UMLPlugin.getEPackageNsURIToProfileLocationMap().get(OpenAPIProfilePackage.eNS_URI);
 		private static final EObject OPENAPI_PROFILE_EPACKAGE = new ResourceSetImpl().getResource(OPENAPI_PROFILE_URI, true).getEObject(OPENAPI_PROFILE_URI.fragment());
 
-		protected static Stereotype getStereotype(EClass definition) {
+		protected static Stereotype getStereotype(EClass definition, ResourceSet resourceSet) {
+//			 EObject OPENAPI_PROFILE_EPACKAGE = resourceSet.getResource(OPENAPI_PROFILE_URI, true).getEObject(OPENAPI_PROFILE_URI.fragment());
 			NamedElement namedElement = UMLUtil.getNamedElement(definition, OPENAPI_PROFILE_EPACKAGE);
 			return namedElement instanceof Stereotype ? (Stereotype) namedElement : null;
 		}
 	
 	}
-	private static Stereotype getStereotype(EClass definition) {
-		return InternalUMLUtil.getStereotype(definition);
+	private static Stereotype getStereotype(EClass definition, ResourceSet resourceSet) {
+		return InternalUMLUtil.getStereotype(definition, resourceSet);
 	}
-	public static String getStereotypeQn(EClass definition) {
-		Stereotype stereotype = getStereotype(definition);
+	public static String getStereotypeQn(EClass definition, ResourceSet resourceSet) {
+		Stereotype stereotype = getStereotype(definition,resourceSet);
 		return stereotype != null ? stereotype.getQualifiedName() : null; //$NON-NLS-1$
 	}
 }
