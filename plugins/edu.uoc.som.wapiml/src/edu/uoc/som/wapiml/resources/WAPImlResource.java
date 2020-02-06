1af435f800dcd0c655382b6951967f59889c2f59
Hamza Ed-doubi <hed-douibi@uoc.edu>
2020-02-06 14:44:27 +0100

    Save the profile using pathmap

--- plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/resources/WAPImlResource.java
+++ plugins/edu.uoc.som.wapiml/src/edu/uoc/som/wapiml/resources/WAPImlResource.java
@@ -12,7 +12,7 @@ import edu.uoc.som.openapi2.profile.OpenAPIProfilePackage;
 import edu.uoc.som.wapiml.utils.IOUtils;
 
 public class WAPImlResource {
-	
+
 	private static ResourceSet umlResourceSet;
 
 	private WAPImlResource() {
@@ -27,16 +27,16 @@ public class WAPImlResource {
 			boolean standalone;
 			// Check if we are inside eclipse or in a standalone mode
 			try {
-				umlResourceSet.getResource(
-						URI.createPlatformPluginURI("edu.uoc.som.openapi2.profile/resources/openapi.profile.uml", true),
-						true);
+
+				URI.createPlatformPluginURI("edu.uoc.som.openapi2.profile/resources/openapi.profile.uml", true);
+
 				standalone = false;
 			} catch (Exception e) {
 				standalone = true;
 			}
-			
+
 			if (standalone) {
-				Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap( ).put(UMLResource.FILE_EXTENSION,
+				Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(UMLResource.FILE_EXTENSION,
 						UMLResource.Factory.INSTANCE);
 				URI baseUri = IOUtils.getResourcesURI();
 				URI umlBaseUri = baseUri.appendSegment("uml");
@@ -48,18 +48,19 @@ public class WAPImlResource {
 						umlBaseUri.appendSegment("metamodels").appendSegment(""));
 				umlResourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.PROFILES_PATHMAP),
 						umlBaseUri.appendSegment("profiles").appendSegment(""));
-				umlResourceSet.getURIConverter().getURIMap().put(
-						URI.createURI("pathmap://OPENAPI_PROFILES/"),
+				umlResourceSet.getURIConverter().getURIMap().put(URI.createURI("pathmap://OPENAPI_PROFILES/"),
 						openAPIBaseUri.appendSegment(""));
-				UMLPlugin.getEPackageNsURIToProfileLocationMap().put(OpenAPIProfilePackage.eNS_URI, URI.createURI("pathmap://OPENAPI_PROFILES/openapi.profile.uml").appendFragment("_8zK3wHyGEemaV87q0fd26g"));
-
-
+				UMLPlugin.getEPackageNsURIToProfileLocationMap().put(OpenAPIProfilePackage.eNS_URI,
+						URI.createURI("pathmap://OPENAPI_PROFILES/openapi.profile.uml")
+								.appendFragment("_8zK3wHyGEemaV87q0fd26g"));
 
 			} else {
-				
-				umlResourceSet.getURIConverter().getURIMap().put(
-						URI.createURI("pathmap://UML_LIBRARIES/UMLPrimitiveTypes.library.uml"), URI.createPlatformPluginURI(
-								"org.eclipse.uml2.uml.resources/libraries/UMLPrimitiveTypes.library.uml", true));
+
+				umlResourceSet.getURIConverter().getURIMap()
+						.put(URI.createURI("pathmap://UML_LIBRARIES/UMLPrimitiveTypes.library.uml"),
+								URI.createPlatformPluginURI(
+										"org.eclipse.uml2.uml.resources/libraries/UMLPrimitiveTypes.library.uml",
+										true));
 				umlResourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.LIBRARIES_PATHMAP),
 						URI.createPlatformPluginURI("org.eclipse.uml2.uml.resources", true).appendSegment("libraries")
 								.appendSegment(""));
@@ -70,10 +71,9 @@ public class WAPImlResource {
 						URI.createPlatformPluginURI("org.eclipse.uml2.uml.resources", true).appendSegment("profiles")
 								.appendSegment(""));
 				umlResourceSet.getURIConverter().getURIMap().put(
-						URI.createURI("pathmap://OPENAPI_PROFILES/openapi.profile.uml"),
-						URI.createPlatformPluginURI("edu.uoc.som.openapi2.profile/resources/openapi.profile.uml", true));
+						URI.createURI("pathmap://OPENAPI_PROFILES/openapi.profile.uml"), URI.createPlatformPluginURI(
+								"edu.uoc.som.openapi2.profile/resources/openapi.profile.uml", true));
 			}
-
 
 		}
 		return umlResourceSet;
